040016d0028a930fa832466622c14cb2f91a7145
agung22ti <agung22ti@mahasiswa.pcr.ac.id>
2024-07-01 15:39:01 +0700

    setup awal

---
+++ database/migrations/2024_07_01_082827_create_donasis_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('donasis', function (Blueprint $table) {
+            $table->id('id_donasi');
+            $table->unsignedBigInteger('id_donatur');
+            $table->foreign('id_donatur')->references('id_user')->on('users');
+            $table->unsignedBigInteger('id_mahasiswa');
+            $table->foreign('id_mahasiswa')->references('id_user')->on('users');
+            $table->unsignedBigInteger('id_permintaan');
+            $table->foreign('id_permintaan')->references('id_permintaan')->on('permintaans');
+            $table->String('jenis_donasi');
+            $table->string('nama_barang');
+            $table->string('deskripsi');
+            $table->string('kuantitas');
+            $table->string('metode_pengiriman');            
+            $table->string('bukti_pengiriman');            
+            $table->string('bukti_penerima');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('donasis');
+    }
+};
